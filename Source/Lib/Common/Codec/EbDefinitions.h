36c8b5d4ee6eb291f805ec5684a5c89914ae8387
hguermaz <hassen.guermazi@intel.com>
2020-08-27 23:09:04 -0700

    MDS2_ALL

--- Source/Lib/Common/Codec/EbDefinitions.h
+++ Source/Lib/Common/Codec/EbDefinitions.h
@@ -698,7 +698,7 @@ extern "C" {
 #if EVALUATE_MDS2
 #define MDS2_TXT 1
 #define MDS2_TXS 1
-#define MDS2_RDOQ 0
+#define MDS2_RDOQ 1
 #endif
 #define MDS2_CLASS_0_3_ONLY 0
 #define MDS2_CLASS_1_2_ONLY 0
