8cb1a73680a8c7fff8c912c6e7f6de5d298c7595
Omar <omar.khlif@intel.com>
2020-08-31 17:28:49 -0700

    remove conidtions pnumber

--- Source/Lib/Encoder/Codec/EbInitialRateControlProcess.c
+++ Source/Lib/Encoder/Codec/EbInitialRateControlProcess.c
@@ -1436,20 +1436,19 @@ void tpl_mc_flow_dispenser(
 #endif
                     uint32_t cu_origin_x = sb_params->origin_x + blk_stats_ptr->origin_x;
                     uint32_t cu_origin_y = sb_params->origin_y + blk_stats_ptr->origin_y;
-                    if(eob) {
-                        if (pcs_ptr->picture_number == 108) {//96
-                            if (cu_origin_x == 304 && cu_origin_y == 128) {
-                                printf("\n^ x %d y %d s %d m %d  row %d col %d^", cu_origin_x, cu_origin_y, bsize, best_mode , final_best_mv.row,final_best_mv.col);
-                                for (int32_t r = 0; r < 16; ++r) {
-                                    for (int32_t c = 0; c < 16; ++c) {
+                    //if (pcs_ptr->picture_number == 108) {//96
+                        // if (cu_origin_x == 304 && cu_origin_y == 128) {
+                            printf("\n^ x %d y %d s %d m %d  row %d col %d^", cu_origin_x, cu_origin_y, bsize, best_mode , final_best_mv.row,final_best_mv.col);
+                            for (int32_t r = 0; r < 16; ++r) {
+                                for (int32_t c = 0; c < 16; ++c) {
+                                    if (dst_buffer[r * dst_buffer_stride + c]> 254/* 0xFE*/) {
                                         printf("\n-%d,%d", c, r);
-                                        if (dst_buffer[r * dst_buffer_stride + c]> 254/* 0xFE*/) {
-                                            printf("\n");
-                                        }
                                     }
                                 }
+                            //   }
+                        // }
                             }
-                        }
+                    if(eob) {
                         av1_inv_transform_recon8bit((int32_t*)dqcoeff, dst_buffer, dst_buffer_stride, dst_buffer, dst_buffer_stride, TX_16X16, DCT_DCT, PLANE_TYPE_Y, eob, 0);
                     }
 
